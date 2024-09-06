6b260ad99aa6702d0c7becdcc13d875e4d622eb3
bambangj <bambangjayasantika23@gmail.com>
2024-09-06 22:44:42 +0700

    form interface

---
+++ src/decimal_convert/Decimal_convert.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package decimal_convert;
+
+/**
+ *
+ * @author bams
+ */
+public class Decimal_convert {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface inter = new Interface();
+        inter.setVisible(true);
+    }
+    
+}
